5f0dd455dea1792cc2042ec5283c4e73c610e9b6
Rebekah <rebekahmenn@gmail.com>
2023-09-22 13:44:04 -0400

    navbar css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Footer />
     </div>
   );
 }
