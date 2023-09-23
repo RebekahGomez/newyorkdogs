f7ed927ef3b77c5dd6249ec703120fd4e2bf61e9
Rebekah <rebekahmenn@gmail.com>
2023-09-23 17:39:48 -0400

    update AllDogs css, added grid, basic styling

---
+++ src/components/AllDogs/AllDogs.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from "react";
+import "./AllDogs.css";
+
+const ImageGallery = () => {
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    // Define a function to fetch images from the JSON file
+    const fetchImages = async () => {
+      try {
+        const response = await fetch("/images.json");
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const imageData = await response.json();
+        setImages(imageData);
+      } catch (error) {
+        console.error("Error fetching images:", error);
+      }
+    };
+
+    // Call the fetchImages function
+    fetchImages();
+  }, []);
+
+  return (
+    <div className="image-gallery">
+      {images.map((image) => (
+        <img src={image.path} alt="" key={image.path} />
+      ))}
+    </div>
+  );
+};
+
+export default ImageGallery;
