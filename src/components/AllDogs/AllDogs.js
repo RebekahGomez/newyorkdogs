3394e27a3a5792cb2426621e9df22f67cc6e104e
Rebekah <rebekahmenn@gmail.com>
2023-11-26 17:44:21 -0500

    added Previous and Next buttons to modal

--- src/components/AllDogs/AllDogs.js
+++ src/components/AllDogs/AllDogs.js
@@ -5,6 +5,7 @@ import "./AllDogs.css";
 const ImageGallery = () => {
   const [images, setImages] = useState([]);
   const [selectedImage, setSelectedImage] = useState(null);
+  const [currentIndex, setCurrentIndex] = useState(null);
 
   useEffect(() => {
     // Define a function to fetch images from the JSON file
@@ -25,30 +26,53 @@ const ImageGallery = () => {
     fetchImages();
   }, []);
 
-  const handleImageClick = (image) => {
+  const handleImageClick = (image, index) => {
     setSelectedImage(image);
+    setCurrentIndex(index);
     document.body.style.overflow = "hidden";
   };
 
   const handleCloseModal = () => {
     setSelectedImage(null);
     document.body.style.overflow = "auto";
+    setCurrentIndex(null);
+  };
+
+  const goToNext = () => {
+    const newIndex = currentIndex + 1;
+    if (newIndex < images.length) {
+      setSelectedImage(images[newIndex]);
+      setCurrentIndex(newIndex);
+    }
+  };
+
+  const goToPrevious = () => {
+    const newIndex = currentIndex - 1;
+    if (newIndex >= 0) {
+      setSelectedImage(images[newIndex]);
+      setCurrentIndex(newIndex);
+    }
   };
 
   return (
     <div>
       <div className="image-gallery">
-        {images.map((image) => (
+        {images.map((image, index) => (
           <img
             src={image.path}
             alt=""
             key={image.id}
             loading="lazy"
-            onClick={() => handleImageClick(image)}
+            onClick={() => handleImageClick(image, index)}
           />
         ))}
       </div>
-      <DogModal image={selectedImage} onClose={handleCloseModal} />
+      <DogModal
+        image={selectedImage}
+        onClose={handleCloseModal}
+        onNext={goToNext}
+        onPrevious={goToPrevious}
+      />
     </div>
   );
 };
--- src/components/DogModal/DogModal.js
+++ src/components/DogModal/DogModal.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./DogModal.css";
 
-const DogModal = ({ image, onClose }) => {
+const DogModal = ({ image, onClose, onNext, onPrevious }) => {
   if (!image) return null;
 
   return (
@@ -10,6 +10,12 @@ const DogModal = ({ image, onClose }) => {
         <img src={image.path} alt="" />
         <button onClick={onClose} className="modal-close-button">
           Close
+        </button>
+        <button onClick={onPrevious} className="modal-nav-button previous">
+          Back
+        </button>
+        <button onClick={onNext} className="modal-nav-button next">
+          Next
         </button>
       </div>
     </div>
