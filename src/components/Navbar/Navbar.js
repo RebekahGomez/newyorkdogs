8035bd2c686b83c1ec9cbaa6630f7f726f3a506d
Rebekah <rebekahmenn@gmail.com>
2023-11-26 19:18:04 -0500

    changed NavBar welcome message

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -6,8 +6,11 @@ const Navbar = () => {
     <nav className="navbar">
       <h1>Dogs of New York</h1>
       <div className="links">
-        Welcome! This is a Work In Paw-gress! Enjoy all these good dogs spotted
-        in NYC as I gradually build out this site.
+        Welcome! This is a <b>Work In Paw-gress!</b> <br />
+        It's probably loading slowly, so just give it a bit. <br />
+        Enjoy all these good dogs spotted in NYC as I gradually build out this
+        site. <br />I appreciate your patience as I learn new techniques to make
+        this a better, more interactive site!
       </div>
     </nav>
   );
