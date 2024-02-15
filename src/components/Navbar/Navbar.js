3b0f5ace4fdee5503f2f62cfd6500acd96ef4a3d
Rebekah <rebekahmenn@gmail.com>
2024-02-15 11:37:44 -0500

    updated header on main branch

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -7,7 +7,8 @@ const Navbar = () => {
       <h1>Dogs of New York</h1>
       <div className="links">
         Welcome! This is a <b>Work In Paw-gress!</b> <br />
-        It's probably loading slowly, so just give it a bit. <br />
+        I'm learning how to solve latency issues, so I appreicate your patience
+        as this loads. <br />
         Enjoy all these good dogs spotted in NYC as I gradually build out this
         site. <br />I appreciate your patience as I learn new techniques to make
         this a better, more interactive site!
